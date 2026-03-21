a48e23c5c34d19d13aaf65489f5c60abff9d88bf
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-21 17:55:52 -0400

    updated merge sort algo
    the loop assigns elements to the sorted array

--- MergeSortAlgorithm.py
+++ MergeSortAlgorithm.py
@@ -3,4 +3,19 @@ def merge_sort(array):
     left_part = array[:middle_point]
     right_part = array[middle_point:]
     
+    # Recursion
+    merge_sort(left_part)
+    merge_sort(right_part)
+
+    left_array_index = 0 # Help keep track of index
+    right_array_index = 0 # Help keep track of index
+    sorted_index = 0 # Help keep track of index
     
+    # Assign elements to the sorted array
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index +=1
