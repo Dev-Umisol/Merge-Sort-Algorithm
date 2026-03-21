fa3f83031e8eb95a4f97917df9f3e243eab786de
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-21 18:49:05 -0400

    Finished mergesort algo

--- MergeSortAlgorithm.py
+++ MergeSortAlgorithm.py
@@ -1,4 +1,8 @@
 def merge_sort(array):
+    # Base Case
+    if len(array) <= 1:
+        return
+    
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
@@ -19,3 +23,24 @@ def merge_sort(array):
         else:
             array[sorted_index] = right_part[right_array_index]
             right_array_index +=1
+        
+        sorted_index +=1 # <-- move next in the index
+    
+    # Left or right might still have elements in them while the other side won't i.e. left might have elements left in the group, but right might not, or vice versa
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+    
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: ')
+    print(numbers)
